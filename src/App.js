6d84ac766e9380c0f83117ce43c476d0a504f1d1
A satya <you@example.com>
2025-09-08 12:36:06 +0530

    pushed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-3xl font-bold">Hey Iam just comes here to learn react more</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
